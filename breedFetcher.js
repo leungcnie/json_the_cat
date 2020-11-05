5319a956e9369569a0f7d39671a0d17a683b4f4f
Connie <leungcnie@gmail.com>
2020-11-05 22:11:47 +0000

    Added functionality to handle request failing

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,12 +4,13 @@ const name = process.argv[2];
 const URL = `https://api.thecatapi.com/v1/breeds/search?q=${name}`;
 
 request(URL, (err, response, body) => {
-  // body object that's value is an array of an object
-  const data = JSON.parse(body);
-  if (data.length !== 0) {
-    const description = data[0].description
+  if (err) console.log("Oops! An error occurred:\n", err);
+
+  const data = JSON.parse(body);   // data is obj with "body" key and value is an array of an object
+  if (data.length !== 0 && data !== undefined) {
+    const description = data[0].description;
     console.log(description);
   } else {
-    console.log(`Breed ${name} not found`);
+    console.log(`Breed ${name} not found`); // if breed not found
   }
-})
+});
