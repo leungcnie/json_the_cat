f63c4b2ec00b62ad69489c110665bf5c40e47e93
Connie <leungcnie@gmail.com>
2020-11-05 23:10:26 +0000

    Fixed function so it handles an invalid URL input into the request

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,12 +2,14 @@ const request = require('request');
 
 const name = process.argv[2];
 const URL = `https://api.thecatapi.com/v1/breeds/search?q=${name}`;
+const typo = 'https://api.thecatapo.com/' // test if typo domain is handled
+const keysmash = "ajdshj"; // test if keysmash URL is handled
 
 request(URL, (err, response, body) => {
-  if (err) console.log("Oops! An error occurred:\n", err);
+  if (err || response === undefined) console.log("Oops! An error occurred:\n", err);
 
   const data = JSON.parse(body);   // data is obj with "body" key and value is an array of an object
-  if (data.length !== 0 && data !== undefined) {
+  if (data.length !== 0) {
     const description = data[0].description;
     console.log(description);
   } else {
